b1013ff1a3bfcff0292e05eb2ac66c133db19681
taetae <peacepiece7@naver.com>
2022-11-15 23:29:18 +0900

    20221115 study 2

---
+++ algorithm/2870_수학숙제/main.cpp
@@ -0,0 +1,48 @@
+/*
+https://www.acmicpc.net/problem/2870
+
+- 문자열 파싱, 구현
+
+예제 입력 1 
+2
+lo3za4
+01
+예제 출력 1 
+1
+3
+4
+
+예제 입력 2 
+4
+43silos0
+zita002
+le2sim
+231233
+예제 출력 2 
+0
+2
+2
+43
+231233
+
+예제 입력 3 
+4
+01bond
+02james007
+03bond
+04austinpowers000
+예제 출력 3 
+0
+1
+2
+3
+4
+7
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+}
