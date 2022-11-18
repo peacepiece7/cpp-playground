4e0bcfec5ac1dc2a027c29edcf849720045b4d8c
taetae <peacepiece7@naver.com>
2022-11-18 15:22:37 +0900

    test : git ignore file test

---
+++ algorithm/1018_체스판다시칠하기/test.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> v{1,2,3};
+    for(auto& i : v) cout << i << " ";
+    cout << "Hello, world!" << endl;
+    return 0;
+}
---
+++ algorithm/tsetfolder/t.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> v{1,2,3};
+    int a = 2;
+    int b = 3;
+    int c = 5;
+    for(auto& i : v) cout << i << " ";
+    cout << "Hello, world!" << endl;
+    return 0;
+}
